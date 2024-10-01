9185f7e06829de8a015be87d35f61e673994aed0
plugar16 <plugarmihai16@gmail.com>
2024-10-01 15:02:50 +0300

    Upload new files

--- camera.js
+++ camera.js
@@ -1,12 +1,14 @@
 var video = document.getElementById("video");
+video.addEventListener("touchstart",capteaza);
+video.addEventListener("mousedown",capteaza);
 //-----------------------------------------------
-function on_cam_success(stream){
-video.srcObject = stream;
+    function on_cam_success(stream){
+    video.srcObject = stream;
 }
 //-----------------------------------------------
 function on_cam_error(err)
 {
-alert("error."+err.message);
+    alert("error."+err.message);
 }
 //-----------------------------------------------
 var constraints = { audio: false, video: true };
@@ -16,10 +18,10 @@ navigator.mediaDevices.getUserMedia(constraints)
 //-----------------------------------------------
 function capteaza()
 {
-var c = document.getElementById("canvas");
-c.width = video.width;
-c.height = video.height;
-var ctx = c.getContext("2d");
-ctx.drawImage(video, 0, 0, 640, 480);
+    var c = document.getElementById("canvas");
+    c.width = video.width;
+    c.height = video.height;
+    var ctx = c.getContext("2d");
+    ctx.drawImage(video, 0, 0, 640, 480);
 }
 //-----------------------------------------------
