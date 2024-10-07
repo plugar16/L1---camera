0110736bfc98c97cf6028abf7b03e04bfdd26f84
plugar16 <plugarmihai16@gmail.com>
2024-10-07 16:27:25 +0300

    Add files via upload

---
+++ orientare3.js
@@ -0,0 +1,22 @@
+window.addEventListener("deviceorientation", on_device_orientation);
+
+function on_device_orientation(evt) 
+{
+    var alpha = evt.alpha;
+    var beta = evt.beta;
+    var gamma = evt.gamma;
+
+    document.getElementById("a").innerHTML = "alpha = " + alpha;
+    document.getElementById("b").innerHTML = "beta = " + beta;
+    document.getElementById("c").innerHTML = "gamma = " + gamma;
+
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+
+    var latime_dreptunghi = 50 + (gamma + 90) * (150 / 180);//variaza intre 50 - 200 pixeli
+
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = "#6A0888";
+    context.beginPath();
+    context.fillRect(180, 150, latime_dreptunghi, 50);
+}
