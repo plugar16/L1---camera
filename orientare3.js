bd5bccb8acde8f4bf2de483aa2b6d3557d7885f5
plugar16 <plugarmihai16@gmail.com>
2024-10-07 16:46:41 +0300

    Add files via upload

--- orientare3.js
+++ orientare3.js
@@ -13,12 +13,10 @@ function on_device_orientation(evt)
     var canvas = document.getElementById("canvas");
     var context = canvas.getContext("2d");
 
-    // var latime_dreptunghi = 50 + (gamma + 90) * (150 / 180);//variaza intre 50 - 200 pixeli
+    var latime_dreptunghi = 50 + (gamma + 90) * (150 / 180);//variaza intre 50 - 200 pixeli
 
-    var inaltime_dreptunghi = 50 + (gamma + 90) * (150 / 180);
-    
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.fillStyle = "#6A0888";
     context.beginPath();
-    context.fillRect(180, 150, 100, inaltime_dreptunghi);
+    context.fillRect(180, 150, latime_dreptunghi, 50);
 }
